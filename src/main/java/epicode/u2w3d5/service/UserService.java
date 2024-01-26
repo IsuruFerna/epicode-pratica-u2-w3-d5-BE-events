d5fe1db80c8f5eaaf893041df881a7696b0a098a
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-01-26 16:18:36 +0100

    user authentication created

--- src/main/java/epicode/u2w3d5/service/UserService.java
+++ src/main/java/epicode/u2w3d5/service/UserService.java
@@ -1,11 +1,7 @@
 package epicode.u2w3d5.service;
 
-import epicode.u2w3d5.entities.Role;
 import epicode.u2w3d5.entities.User;
-import epicode.u2w3d5.exceptions.BadRequestException;
 import epicode.u2w3d5.exceptions.NotFoundException;
-import epicode.u2w3d5.payload.user.NewUserDTO;
-import epicode.u2w3d5.payload.user.NewUserResponse;
 import epicode.u2w3d5.repository.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,18 +19,6 @@ public class UserService {
         return userDAO.findAll();
     }
 
-    public NewUserResponse save(NewUserDTO user) {
-        userDAO.findByEmail(user.email()).ifPresent(found -> {
-            throw new BadRequestException("Email " + found.getEmail() + " is already exsist!");
-        });
-        User u = new User();
-        u.setEmail(user.email());
-        u.setFirstname(user.firstName());
-        u.setSurname(user.lastName());
-        u.setPassword(user.password());
-        u.setRole(Role.USER);
-        return new NewUserResponse(u.getId());
-    }
 
     public User findById(UUID id) {
         return userDAO.findById(id).orElseThrow(() -> new NotFoundException(id));
@@ -52,5 +36,9 @@ public class UserService {
         found.setEmail(body.getEmail());
 
         return userDAO.save(found);
+    }
+
+    public User findByEmail(String email) throws NotFoundException {
+        return userDAO.findByEmail(email).orElseThrow(() -> new NotFoundException("User with email " + email + " not found!"));
     }
 }
